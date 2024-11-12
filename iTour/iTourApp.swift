cda6430e50ff167b6954d1810b8fb4da2276c9d3
David Malicke <davemalicke@gmail.com>
2024-11-12 18:38:50 -0500

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by David Malicke on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
