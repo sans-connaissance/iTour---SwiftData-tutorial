40b05bb2684d9b1dc083f6e0dc8fb284e2b90667
David Malicke <davemalicke@gmail.com>
2024-11-13 22:14:20 -0500

    Add sorting

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -5,17 +5,56 @@
 //  Created by David Malicke on 11/12/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    /// Methods for when you know sort order at compile time
+    //   @Query(sort: \Destination.priority, order: .reverse) var destinations: [Destination]
+    //    @Query(sort: [SortDescriptor(\Destination.priority, order: .reverse), SortDescriptor(\Destination.name)]) var destinations: [Destination]
+    @State private var path = [Destination]()
+    @State private var sortOrder = SortDescriptor(\Destination.name)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $path) {
+            DestinationListView(sort: sortOrder)
+                .navigationTitle("iTour")
+                .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .toolbar {
+                    Button("Add Samples", action: addSamples)
+                    Button("Add Destination", systemImage: "plus" , action: addDestination)
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Name")
+                                .tag(SortDescriptor(\Destination.name))
+                            
+                            Text("Priority")
+                                .tag(SortDescriptor(\Destination.priority, order: .reverse))
+                            
+                            Text("Date")
+                                .tag(SortDescriptor(\Destination.date))
+                        }
+                        .pickerStyle(.inline)
+                    }
+                }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+        
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
+    }
+    
+    func addDestination() {
+        let destination = Destination()
+        modelContext.insert(destination)
+        path = [destination]
     }
 }
 
---
+++ iTour/DestinationListView.swift
@@ -0,0 +1,44 @@
+//
+//  DestinationListView.swift
+//  iTour
+//
+//  Created by David Malicke on 11/13/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DestinationListView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: [SortDescriptor(\Destination.priority, order: .reverse), SortDescriptor(\Destination.name)]) var destinations: [Destination]
+    var body: some View {
+        List {
+            ForEach(destinations) { destination in
+                NavigationLink(value: destination) {
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text (destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .onDelete(perform: deleteDestination)
+        }
+    }
+    
+    init(sort: SortDescriptor<Destination>) {
+        _destinations = Query(sort: [sort])
+    }
+    
+    func deleteDestination(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+}
+
+#Preview {
+    DestinationListView(sort: SortDescriptor(\Destination.name))
+}
